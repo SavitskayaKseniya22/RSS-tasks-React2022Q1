6d511f3de65de68805fc5fad17bb5e128ab15481
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2022-03-27 00:11:16 +0300

    feat: add state for favorite card

--- react.components/src/components/Card.tsx
+++ react.components/src/components/Card.tsx
@@ -4,29 +4,42 @@ import phoneImg from '../assets/svg/phone.svg';
 import linkImg from '../assets/svg/forward-next-arrow.svg';
 import tempImgHouse from '../assets/svg/temp-house.jpg';
 import likeImg from '../assets/svg/like.svg';
+import likeImgFavorite from '../assets/svg/like-favorite.svg';
 
 import './card.css';
 
-export class Card extends React.Component<CardProps> {
+export class Card extends React.Component<CardProps, { isFavorite: boolean }> {
+  static defaultProps: CardProps;
   constructor(props: CardProps) {
     super(props);
+
+    this.state = {
+      isFavorite: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+    this.setState((state) => ({
+      isFavorite: !state.isFavorite,
+    }));
+    (e.target as HTMLButtonElement).classList.toggle('favorite-add');
+  }
+
   render() {
     return (
       <li className="Card">
         <figure className="Card__main-img">
-          <img src={this.props.src || tempImgHouse} alt="lot img" />
+          <img src={this.props.img} alt="lot img" />
           <h3>
-            <strong>{this.props.imgDescription || 'Ut enim ad minim veniam'}</strong>
+            <strong>{this.props.name}</strong>
           </h3>
           <figcaption>
-            <i>{this.props.lotAdress || '3014 Tree Frog Lane, Lenexa, Missouri'}</i>
+            <i>{this.props.adress}</i>
           </figcaption>
         </figure>
-        <p className="Card__description">
-          {this.props.lotDescription ||
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-        </p>
+        <p className="Card__description">{this.props.description}</p>
         <div className="Card__links">
           <a href={'tel:' + this.props.phone} className="Card__phone">
             <img src={phoneImg} alt="call me" />
@@ -35,14 +48,14 @@ export class Card extends React.Component<CardProps> {
             <img src={emailImg} alt="email me" />
           </a>
           <a
-            href={this.props.link || '#'}
+            href={this.props.link}
             target="_blank"
             className="Card__link-to-order"
             rel="noreferrer"
           >
             <img src={linkImg} alt="go to site" />
           </a>
-          <button className="Card__mark-like">
+          <button className="Card__mark-like" onClick={this.handleClick}>
             <img src={likeImg} alt="like" />
           </button>
         </div>
@@ -51,11 +64,22 @@ export class Card extends React.Component<CardProps> {
   }
 }
 
+Card.defaultProps = {
+  img: tempImgHouse,
+  adress: '3014 Tree Frog Lane, Lenexa, Missouri',
+  name: 'Ut enim ad minim veniam',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  link: '#',
+  email: '',
+  phone: '',
+};
+
 interface CardProps {
-  src?: string;
-  lotAdress?: string;
-  imgDescription?: string;
-  lotDescription?: string;
+  img?: string;
+  adress?: string;
+  name?: string;
+  description?: string;
   link?: string;
   email?: string;
   phone?: string;
