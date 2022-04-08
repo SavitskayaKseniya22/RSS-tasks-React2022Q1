1342a4b0e105b30a783f3a5e4c691aca5df49c4d
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2022-04-08 21:54:02 +0300

    feat: add validation for other inputs

--- react.components/src/components/AddMyAdvForm.tsx
+++ react.components/src/components/AddMyAdvForm.tsx
@@ -31,12 +31,22 @@ export class AddAdv extends React.Component<Record<string, never>, FormTypes> {
     this.ready = React.createRef();
     this.currency = React.createRef();
     this.submit = React.createRef();
-    this.state = { isValidTitle: null };
+    this.state = {
+      isValidTitle: null,
+      isValidDescription: null,
+      isValidTel: null,
+      isValidEmail: null,
+      isValidArea: null,
+      isValidPrice: null,
+      isValidDate: null,
+    };
   }
 
   handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    this.validation();
+    if (!this.validation()) {
+      this.submit!.current!.setAttribute('disabled', 'true');
+    }
   }
 
   handleInput(event: FormEvent<HTMLInputElement>) {
@@ -47,11 +57,64 @@ export class AddAdv extends React.Component<Record<string, never>, FormTypes> {
   }
 
   validation() {
-    if (/[A-Za-z0-9\s]{6,}/.test(this.title.current?.value as string)) {
+    let isCorrect = false;
+    if (/^[A-Za-z0-9\s]{6,}$/.test(this.title.current?.value as string)) {
       this.setState({ isValidTitle: true });
+      isCorrect = true;
     } else {
       this.setState({ isValidTitle: false });
+      isCorrect = false;
     }
+
+    if (/^[A-Za-z0-9\s]{10,}$/.test(this.description.current?.value as string)) {
+      this.setState({ isValidDescription: true });
+      isCorrect = true;
+    } else {
+      this.setState({ isValidDescription: false });
+      isCorrect = false;
+    }
+
+    if (/^[0-9\s-]{5,}$/.test(this.tel.current?.value as string)) {
+      this.setState({ isValidTel: true });
+      isCorrect = true;
+    } else {
+      this.setState({ isValidTel: false });
+      isCorrect = false;
+    }
+
+    if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(this.email.current?.value as string)) {
+      this.setState({ isValidEmail: true });
+      isCorrect = true;
+    } else {
+      this.setState({ isValidEmail: false });
+      isCorrect = false;
+    }
+
+    if (/^[0-9]*[.,]?[0-9]+$/.test(this.area.current?.value as string)) {
+      this.setState({ isValidArea: true });
+      isCorrect = true;
+    } else {
+      this.setState({ isValidArea: false });
+      isCorrect = false;
+    }
+
+    if (/^[0-9]*[.,]?[0-9]+$/.test(this.price.current?.value as string)) {
+      this.setState({ isValidPrice: true });
+      isCorrect = true;
+    } else {
+      this.setState({ isValidPrice: false });
+      isCorrect = false;
+    }
+
+    if (new Date(this.date.current?.value as string) > new Date()) {
+      this.setState({ isValidDate: true });
+      isCorrect = true;
+    } else {
+      this.setState({ isValidDate: false });
+      isCorrect = false;
+    }
+
+    return isCorrect;
   }
 
   resetValidation(input: string) {
@@ -62,7 +125,37 @@ export class AddAdv extends React.Component<Record<string, never>, FormTypes> {
         }));
         break;
 
-      case 'value2':
+      case 'description':
+        this.setState((state) => ({
+          isValidDescription: state.isValidDescription || !state.isValidDescription,
+        }));
+        break;
+
+      case 'tel':
+        this.setState((state) => ({
+          isValidTel: state.isValidTel || !state.isValidTel,
+        }));
+        break;
+
+      case 'email':
+        this.setState((state) => ({
+          isValidEmail: state.isValidEmail || !state.isValidEmail,
+        }));
+        break;
+      case 'area':
+        this.setState((state) => ({
+          isValidArea: state.isValidArea || !state.isValidArea,
+        }));
+        break;
+      case 'price':
+        this.setState((state) => ({
+          isValidPrice: state.isValidPrice || !state.isValidPrice,
+        }));
+        break;
+      case 'date':
+        this.setState((state) => ({
+          isValidDate: state.isValidDate || !state.isValidDate,
+        }));
         break;
 
       default:
@@ -73,7 +166,7 @@ export class AddAdv extends React.Component<Record<string, never>, FormTypes> {
   render() {
     return (
       <div data-testid="add-adv">
-        <form className="add-adv" onSubmit={this.handleSubmit}>
+        <form className="add-adv" onSubmit={this.handleSubmit} noValidate>
           <label className="big-field">
             <h3>Title:</h3>
             <input type="text" name="title" ref={this.title} onInput={this.handleInput} />
@@ -92,30 +185,59 @@ export class AddAdv extends React.Component<Record<string, never>, FormTypes> {
               type="text"
               name="description"
               ref={this.description}
-              pattern="[A-Za-z0-9\s]{10,}"
+              onInput={this.handleInput}
             />
+            {
+              <span id="description-error">
+                {this.state.isValidDescription === false
+                  ? 'Please enter a valid description. String must contain at least 10 characters.'
+                  : ''}
+              </span>
+            }
           </label>
 
           <label className="middle-field">
             <h3>Tel:</h3>
-            <input type="tel" name="tel" ref={this.tel} pattern="[0-9\s-]{5,}" />
+            <input type="tel" name="tel" ref={this.tel} onInput={this.handleInput} />
+            {
+              <span id="tel-error">
+                {this.state.isValidTel === false
+                  ? 'Please enter a valid number. Number must contain at least 5 digits.'
+                  : ''}
+              </span>
+            }
           </label>
 
           <label className="middle-field">
             <h3>E-mail:</h3>
-            <input type="email" name="email" id="" ref={this.email} />
+            <input type="email" name="email" ref={this.email} onInput={this.handleInput} />
+            {
+              <span id="email-error">
+                {this.state.isValidEmail === false ? 'Please enter a valid email' : ''}
+              </span>
+            }
           </label>
 
           <label className="small-field">
             <h3>Area (&#13217;):</h3>
-            <input type="number" name="area" ref={this.area} pattern="^[0-9]*[.,]?[0-9]+$" />
+            <input type="number" name="area" ref={this.area} onInput={this.handleInput} />
+            {
+              <span id="area-error">
+                {this.state.isValidArea === false ? 'Please enter a valid number' : ''}
+              </span>
+            }
           </label>
 
           <label className="small-field">
             <h3>Price:</h3>
             <div className="price">
-              <input type="number" name="" ref={this.price} pattern="^[0-9]*[.,]?[0-9]+$" />
-              <select name="currency" ref={this.currency}>
+              <input type="number" name="price" ref={this.price} onInput={this.handleInput} />
+              {
+                <span id="price-error">
+                  {this.state.isValidPrice === false ? 'Please enter a valid number' : ''}
+                </span>
+              }
+              <select name="currency" ref={this.currency} defaultValue="₽">
                 <option value="$">$</option>
                 <option value="€">€</option>
                 <option value="€">₽</option>
@@ -124,12 +246,17 @@ export class AddAdv extends React.Component<Record<string, never>, FormTypes> {
           </label>
           <label className="small-field">
             <h3>Date:</h3>
-            <input type="date" name="date" ref={this.date} />
+            <input type="date" name="date" ref={this.date} onInput={this.handleInput} />
+            {
+              <span id="date-error">
+                {this.state.isValidDate === false ? 'Please enter a valid date' : ''}
+              </span>
+            }
           </label>
 
           <label className="small-field">
             <h3>Sale:</h3>
-            <input type="radio" id="sale" name="typeAdd" value="sale" />
+            <input type="radio" id="sale" name="typeAdd" value="sale" defaultChecked />
           </label>
           <label className="small-field">
             <h3>Rent:</h3>
@@ -160,4 +287,10 @@ export class AddAdv extends React.Component<Record<string, never>, FormTypes> {
 
 interface FormTypes {
   isValidTitle: boolean | null;
+  isValidDescription: boolean | null;
+  isValidTel: boolean | null;
+  isValidEmail: boolean | null;
+  isValidArea: boolean | null;
+  isValidPrice: boolean | null;
+  isValidDate: boolean | null;
 }
