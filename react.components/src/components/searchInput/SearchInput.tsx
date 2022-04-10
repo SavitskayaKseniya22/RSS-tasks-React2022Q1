516ab324f95a73647ba68fd83f91b8a093ec6293
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2022-04-10 21:36:00 +0300

    feat: add basic func for api

--- react.components/src/components/searchInput/SearchInput.tsx
+++ react.components/src/components/searchInput/SearchInput.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import './searchInput.css';
 
 export class SearchInput extends React.Component<SearchInputProps, SearchInputState> {
+  search: React.RefObject<HTMLInputElement>;
+
   constructor(props: SearchInputProps) {
     super(props);
-
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.search = React.createRef();
     this.state = {
       input: window.localStorage.getItem('searchValue') || '',
     };
+  }
+  async handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    this.getApiResponse(this.search.current?.value as string);
+  }
 
-    this.handleChange = this.handleChange.bind(this);
+  async getApiResponse(value: string) {
+    const url = `https://api.unsplash.com/search/photos?client_id=ofM-1kx5RC6ZUCCfZy12f78_KZl3oW5gpojrMlT4n4A&per_page=20&query=${value}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
   }
 
   handleChange(e: { target: { value: string } }) {
@@ -24,15 +37,16 @@ export class SearchInput extends React.Component<SearchInputProps, SearchInputSt
 
   render() {
     return (
-      <div>
+      <form className="search-form" onSubmit={this.handleSubmit} data-testid="search-form">
         <input
           data-testid="search-input"
           className="Search-input"
           value={this.state.input}
           onChange={this.handleChange}
+          ref={this.search}
           placeholder="search new home"
         />
-      </div>
+      </form>
     );
   }
 }
